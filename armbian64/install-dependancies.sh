520388f8a613e6352f581d6b21b624c5a0c305a5
triii <chris@triii.org>
2022-11-03 23:42:14 -0400

    add sudo

--- armbian64/install-dependancies.sh
+++ armbian64/install-dependancies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y wget apt-utils pkg-config file gcc git curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 apt-get install -y nodejs gcc g++ make
 echo "NODEJS install complete > please wait.."
 wget https://go.dev/dl/go1.19.2.linux-arm64.tar.gz
